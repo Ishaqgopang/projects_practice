7c37bf5099ac6d5eb45fb034873caf6736890f14
ishaq khan <ishaq.khan.gopang@gmail.com>
2024-11-18 18:39:52 +0500

    weight_conversion

---
+++ assignment.py
@@ -0,0 +1,18 @@
+
+# def double_it():
+#   user_input = int(input("Enter a number"))
+# def double_it():
+#   user_input = int(input("Enter a number"))
+#   while user_input <= 100:
+#       user_input = 2 * user_input
+#       print(user_input)
+# print(user_input , end=" ")
+# double_it()
+
+def double_it():
+    user_input = int(input("Enter Your Number : "))
+    while user_input <100:
+        user_iput = 2 * user_input
+        print(user_input)
+    print(user_input, end=" ")
+double_it()
